ae09005e57329b07809c19d817d11532357d8244
omegaManta <omega_manta@hotmail.com>
2024-05-31 13:52:14 -0500

    deploy

--- backend/src/db/conexion.js
+++ backend/src/db/conexion.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     host: process.env.host  || 'localhost',
     database: process.env.database || 'soporte',
     password: process.env.password || '1234',
-    DB_PORT: process.env.DB_PORT || '5432'   
+    port: process.env.port || '5432'   
 })
 
 module.exports = {
