150003fa18b8f06189cb3c78af225235a3d9024d
omegaManta <omega_manta@hotmail.com>
2024-08-01 21:58:23 -0500

    Deploy

--- backend/src/controlador/informacion.js
+++ backend/src/controlador/informacion.js
@@ -1,8 +1,11 @@
-const {pool} = require('../db/conexion')
+const {pool} = require('../db/conexion');
+const jwt = require('jsonwebtoken');
+
 
 const crearcontacto = async(req,res)=>{
-const {nombre,url} = req.body
-const guardar = await pool.query('insert into social(nombre,url)values($1,$2)',[
+const {idusuario,nombre,url} = req.body
+const guardar = await pool.query('insert into social(idusuario,nombre,url)values($1,$2,$3)',[
+    idusuario,
     nombre,
     url
 ])
@@ -16,8 +19,60 @@ res.json({
 
 
 const vercontactos = async(req,res)=>{
-    const response = await pool.query('select idred,nombre,url from social')
-    res.status(200).json(response.rows)
+    const token = req.headers.authorization;
+      
+    if (!token) {
+      res.status(401).json({ error: 'Token no proporcionado' });
+      return;
+    }
+    
+    try {
+      const decoded = jwt.verify(token, 'sistema omega web');
+      const userId = decoded.userId;
+    
+      pool.query('select s.idred,s.nombre,s.url from social s join usuario u on u.idusuario = s.idusuario join copia c on u.idusuario = c.idusuario where c.idempresa = $1', [userId], (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Error al obtener el perfil del usuario' });
+          return;
+        }
+    
+        const userProfile = result.rows;
+        res.json({ profile: userProfile });
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ error: 'Token inválido' });
+    }
+}
+
+
+const vercontactospanel = async(req,res)=>{
+    const token = req.headers.authorization;
+      
+    if (!token) {
+      res.status(401).json({ error: 'Token no proporcionado' });
+      return;
+    }
+    
+    try {
+      const decoded = jwt.verify(token, 'panel omega web');
+      const userId = decoded.userId;
+    
+      pool.query('select s.idred,s.nombre,s.url from social s join usuario u on u.idusuario = s.idusuario where u.idusuario = $1', [userId], (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Error al obtener el perfil del usuario' });
+          return;
+        }
+    
+        const userProfile = result.rows;
+        res.json({ profile: userProfile });
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ error: 'Token inválido' });
+    }
 }
 
 
@@ -58,6 +113,7 @@ const eliminarcontacto = async(req,res)=>{
 module.exports = {
 crearcontacto,
 vercontactos,
+vercontactospanel,
 vercontacto,
 editarcontacto,
 eliminarcontacto
--- backend/src/rutas/informacion.js
+++ backend/src/rutas/informacion.js
@@ -1,5 +1,5 @@
 const {crearcontacto,
-vercontactos,vercontacto,
+vercontactos,vercontactospanel,vercontacto,
 editarcontacto,eliminarcontacto} = require('../controlador/informacion')
 const {Router} = require('express')
 const router = Router()
@@ -7,6 +7,7 @@ const router = Router()
 
 router.post('/contacto',crearcontacto)
 router.get('/contactos',vercontactos)
+router.get('/contactos-panel',vercontactospanel)
 router.get('/contacto/:idred',vercontacto)
 router.put('/contacto/:idred',editarcontacto)
 router.delete('/contacto/:idred',eliminarcontacto)
