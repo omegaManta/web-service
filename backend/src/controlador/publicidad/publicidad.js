b7991d11981c63423453b753ace9682fad9af837
omegaManta <omega_manta@hotmail.com>
2024-08-06 15:19:56 -0500

    Deploy

--- backend/src/controlador/publicidad/publicidad.js
+++ backend/src/controlador/publicidad/publicidad.js
@@ -1,6 +1,7 @@
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 const {pool} = require('../../db/conexion');
+const jwt = require('jsonwebtoken');
 
 
 const upload = multer({dest: 'uploads'});
@@ -14,8 +15,9 @@ const crearanuncio = async(req,res)=>{
             folder: 'anuncios'
         })
         const archivoUrl = result.secure_url;
-        const {descripcion} = req.body;
-        const guardar = await pool.query('insert into publicidad(archivo,descripcion)values($1,$2)',[
+        const {idusuario,descripcion} = req.body;
+        const guardar = await pool.query('insert into publicidad(idusuario,archivo,descripcion)values($1,$2,$3)',[
+            idusuario,
             archivoUrl,
             descripcion
         ])
@@ -28,14 +30,65 @@ const crearanuncio = async(req,res)=>{
 
 
 const veranuncios = async(req,res)=>{
-const response = await pool.query('select idpubli,archivo,descripcion from publicidad')
+const response = await pool.query('select idpubli,archivo,descripcion,idusuario from publicidad')
 res.status(200).json(response.rows)
 }
 
 const publicidad = async(req,res)=>{
-    const response = await pool.query('select idpubli,archivo,descripcion from publicidad order by fecha_hora desc limit 3')
-    res.status(200).json(response.rows)
+    const token = req.headers.authorization;
+    
+if (!token) {
+  res.status(401).json({ error: 'Token no proporcionado' });
+  return;
+}
+
+try {
+  const decoded = jwt.verify(token, 'sistema omega web');
+  const userId = decoded.userId;
+
+  pool.query('select p.idpubli,p.archivo,p.descripcion from publicidad p join usuario u on u.idusuario = p.idusuario join copia c on u.idusuario = c.idusuario where c.idempresa = $1 order by p.fecha_hora desc limit 3', [userId], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Error al obtener el perfil del usuario' });
+      return;
     }
+
+    const userProfile = result.rows;
+    res.json({ profile: userProfile });
+  });
+} catch (error) {
+  console.error(error);
+  res.status(401).json({ error: 'Token inválido' });
+}
+    }
+
+    const publicidadpanel = async(req,res)=>{
+        const token = req.headers.authorization;
+        
+    if (!token) {
+      res.status(401).json({ error: 'Token no proporcionado' });
+      return;
+    }
+    
+    try {
+      const decoded = jwt.verify(token, 'panel omega web');
+      const userId = decoded.userId;
+    
+      pool.query('select p.idpubli,p.archivo,p.descripcion from publicidad p join usuario u on u.idusuario = p.idusuario  where u.idusuario = $1', [userId], (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Error al obtener el perfil del usuario' });
+          return;
+        }
+    
+        const userProfile = result.rows;
+        res.json({ profile: userProfile });
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ error: 'Token inválido' });
+    }
+        }    
 
 
 const eliminaranuncio = async(req,res)=>{
@@ -53,5 +106,6 @@ module.exports = {
 crearanuncio,
 veranuncios,
 eliminaranuncio,
-publicidad
+publicidad,
+publicidadpanel
 }
