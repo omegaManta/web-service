1c7e829825cb76ffd2f0690f692e319ff4279813
omegaManta <omega_manta@hotmail.com>
2024-08-11 16:01:09 -0500

    Deploy

--- backend/src/controlador/panel/panel.js
+++ backend/src/controlador/panel/panel.js
@@ -154,7 +154,7 @@ const verperfilcorreo = async (req, res) => {
 //tecnicos
 const verpedidovendedor = async(req,res)=>{
   const idempresa = req.params.idempresa;
-  const response = await pool.query('select s.descripcion,count(*),sum(s.precio), c.nombre_empresa,c.telefono, s.precio, s.foto,p.estado from pedido p  join servicio s on s.idservicio = p.idservicio join copia c on c.idempresa = p.idempresa where c.idempresa = $1 group by s.descripcion,c.nombre_empresa,c.telefono,s.precio,s.foto,p.estado',[
+  const response = await pool.query('select s.descripcion,count(*),sum(s.precio2), c.nombre_empresa,c.telefono, s.precio2, s.foto,p.estado from pedido p  join servicio s on s.idservicio = p.idservicio join copia c on c.idempresa = p.idempresa where c.idempresa = $1 group by s.descripcion,c.nombre_empresa,c.telefono,s.precio2,s.foto,p.estado',[
     idempresa
   ]);
   res.status(200).json(response.rows);
